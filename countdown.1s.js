d4ea2e7a02588a63230d7f91a4e48d7745fb3b42
Mark <mark@remarkablemark.org>
2018-04-25 21:30:46 -0400

    feat(plugin): create countdown.1s.js with hardcoded time 7:30:00

---
+++ countdown.1s.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env /usr/local/bin/node
+
+// <bitbar.title>Countdown</bitbar.title>
+// <bitbar.version>v0.1.0</bitbar.version>
+// <bitbar.author>Menglin "Mark" Xu</bitbar.author>
+// <bitbar.author.github>remarkablemark</bitbar.author.github>
+// <bitbar.desc>Counts down the time remaining.</bitbar.desc>
+// <bitbar.dependencies>node</bitbar.dependencies>
+
+/**
+ * Ensures the number has 2 digits.
+ *
+ * @param  {Number} number
+ * @return {Number|String}
+ */
+const pad = number => (number > 9 ? number : '0' + number);
+
+// current time
+const now = Date.now();
+
+// end time
+const end = new Date();
+end.setHours(7);
+end.setMinutes(30);
+end.setSeconds(0);
+end.setMilliseconds(0);
+
+// delta time
+let delta = end - now;
+
+// add 1 day if delta time is negative
+if (delta < 1) {
+  end.setDate(end.getDate() + 1);
+  delta = end - now;
+}
+
+// remaining milliseconds
+// const ms = delta % 1000;
+
+// remaining seconds
+const s = Math.floor(delta / 1000) % 60;
+
+// remaining minutes
+const m = Math.floor(delta / (1000 * 60)) % 60;
+
+// remaining hours
+const h = Math.floor(delta / (1000 * 60 * 60)) % 24;
+
+// display remaining time
+console.log(`${h}:${pad(m)}:${pad(s)}`);
