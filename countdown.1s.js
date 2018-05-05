5f9c29e87a797417f61692df1d3c1f9d07dd7da9
Mark <mark@remarkablemark.org>
2018-05-04 22:45:16 -0400

    feat(plugin): replace hardcoded time and render dropdown

--- countdown.1s.js
+++ countdown.1s.js
@@ -7,17 +7,37 @@
 // <bitbar.desc>Counts down the time remaining.</bitbar.desc>
 // <bitbar.dependencies>node</bitbar.dependencies>
 
+const { resolve } = require('path');
+
 const bitbar = require('./lib/bitbar');
 const { pad } = require('./lib/helpers');
+const { path } = require('./lib/constants');
+
+let H, M, S, time;
+
+try {
+  // get time if set
+  const data = require('./tmp/data');
+  [H, M, S] = data.time.split(':');
+  S = S || 0;
+  time = `${H}:${pad(M)}:${pad(S)}`;
+
+} catch (err) {
+  // otherwise display unset time and option
+  bitbar.output('00:00:00');
+  bitbar.separator.dropdown;
+  bitbar.output(`Set Time | color=green bash='${path.setTime}' terminal=false`);
+  process.exit();
+}
 
 // current time
 const now = Date.now();
 
 // end time
 const end = new Date();
-end.setHours(7);
-end.setMinutes(30);
-end.setSeconds(0);
+end.setHours(H);
+end.setMinutes(M);
+end.setSeconds(S);
 end.setMilliseconds(0);
 
 // delta time
@@ -42,4 +62,10 @@ const m = Math.floor(delta / (1000 * 60)) % 60;
 const h = Math.floor(delta / (1000 * 60 * 60)) % 24;
 
 // display remaining time
-bitbar.output(`${h}:${pad(m)}:${pad(s)}`);
+const remaining = `${h}:${pad(m)}:${pad(s)}`;
+bitbar.output(remaining);
+
+bitbar.separator.dropdown;
+
+// set time
+bitbar.output(`Set Time (${time}) | color=green bash='${path.setTime}' terminal=false`);
